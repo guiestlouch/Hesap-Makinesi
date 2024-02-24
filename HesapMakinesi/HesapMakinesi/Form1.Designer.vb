a3cbc640ae178dc3b39e2174d4c31f9ba73c6d5a
guiestlouch <134270846+guiestlouch@users.noreply.github.com>
2024-02-24 16:06:50 +0300

    Add files via upload

---
+++ HesapMakinesi/HesapMakinesi/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmHesapMakinesi
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btnBolu = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btnCarpi = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnArti = New Button()
+        btnVirgul = New Button()
+        btn0 = New Button()
+        btnC = New Button()
+        BtnEksi = New Button()
+        btnEsittir = New Button()
+        txtDisplay = New TextBox()
+        btnBackSpace = New Button()
+        btnNegatif = New Button()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn1.Location = New Point(12, 183)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(50, 65)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn2.Location = New Point(68, 183)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(50, 65)
+        btn2.TabIndex = 0
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn3.Location = New Point(124, 183)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(50, 65)
+        btn3.TabIndex = 0
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnBolu
+        ' 
+        btnBolu.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnBolu.Location = New Point(180, 41)
+        btnBolu.Name = "btnBolu"
+        btnBolu.Size = New Size(50, 65)
+        btnBolu.TabIndex = 0
+        btnBolu.Text = "÷"
+        btnBolu.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn4.Location = New Point(12, 112)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(50, 65)
+        btn4.TabIndex = 0
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn5.Location = New Point(68, 112)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(50, 65)
+        btn5.TabIndex = 0
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn6.Location = New Point(124, 112)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(50, 65)
+        btn6.TabIndex = 0
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btnCarpi
+        ' 
+        btnCarpi.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnCarpi.Location = New Point(180, 112)
+        btnCarpi.Name = "btnCarpi"
+        btnCarpi.Size = New Size(50, 65)
+        btnCarpi.TabIndex = 0
+        btnCarpi.Text = "X"
+        btnCarpi.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn7.Location = New Point(12, 41)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(50, 65)
+        btn7.TabIndex = 0
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn8.Location = New Point(68, 41)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(50, 65)
+        btn8.TabIndex = 0
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn9.Location = New Point(124, 41)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(50, 65)
+        btn9.TabIndex = 0
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnArti
+        ' 
+        btnArti.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnArti.Location = New Point(180, 183)
+        btnArti.Name = "btnArti"
+        btnArti.Size = New Size(50, 65)
+        btnArti.TabIndex = 0
+        btnArti.Text = "+"
+        btnArti.UseVisualStyleBackColor = True
+        ' 
+        ' btnVirgul
+        ' 
+        btnVirgul.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnVirgul.Location = New Point(124, 254)
+        btnVirgul.Name = "btnVirgul"
+        btnVirgul.Size = New Size(50, 65)
+        btnVirgul.TabIndex = 0
+        btnVirgul.Text = ","
+        btnVirgul.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btn0.Location = New Point(68, 254)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(50, 65)
+        btn0.TabIndex = 0
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnC.Location = New Point(236, 41)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(50, 65)
+        btnC.TabIndex = 0
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' BtnEksi
+        ' 
+        BtnEksi.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        BtnEksi.Location = New Point(180, 254)
+        BtnEksi.Name = "BtnEksi"
+        BtnEksi.Size = New Size(50, 65)
+        BtnEksi.TabIndex = 0
+        BtnEksi.Text = "-"
+        BtnEksi.UseVisualStyleBackColor = True
+        ' 
+        ' btnEsittir
+        ' 
+        btnEsittir.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnEsittir.Location = New Point(236, 183)
+        btnEsittir.Name = "btnEsittir"
+        btnEsittir.Size = New Size(50, 136)
+        btnEsittir.TabIndex = 0
+        btnEsittir.Text = "="
+        btnEsittir.UseVisualStyleBackColor = True
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Location = New Point(11, 12)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(275, 23)
+        txtDisplay.TabIndex = 1
+        txtDisplay.Text = "0"
+        txtDisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btnBackSpace
+        ' 
+        btnBackSpace.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnBackSpace.Location = New Point(236, 112)
+        btnBackSpace.Name = "btnBackSpace"
+        btnBackSpace.Size = New Size(50, 65)
+        btnBackSpace.TabIndex = 0
+        btnBackSpace.Text = "<="
+        btnBackSpace.UseVisualStyleBackColor = True
+        ' 
+        ' btnNegatif
+        ' 
+        btnNegatif.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold)
+        btnNegatif.Location = New Point(12, 254)
+        btnNegatif.Name = "btnNegatif"
+        btnNegatif.Size = New Size(50, 65)
+        btnNegatif.TabIndex = 0
+        btnNegatif.Text = "+/-"
+        btnNegatif.UseVisualStyleBackColor = True
+        ' 
+        ' frmHesapMakinesi
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(299, 336)
+        Controls.Add(txtDisplay)
+        Controls.Add(BtnEksi)
+        Controls.Add(btnBackSpace)
+        Controls.Add(btnC)
+        Controls.Add(btnArti)
+        Controls.Add(btn9)
+        Controls.Add(btnCarpi)
+        Controls.Add(btnNegatif)
+        Controls.Add(btn0)
+        Controls.Add(btn6)
+        Controls.Add(btn8)
+        Controls.Add(btnBolu)
+        Controls.Add(btnEsittir)
+        Controls.Add(btnVirgul)
+        Controls.Add(btn5)
+        Controls.Add(btn7)
+        Controls.Add(btn3)
+        Controls.Add(btn4)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Name = "frmHesapMakinesi"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Hesap Makinesi"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnBolu As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnCarpi As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnArti As Button
+    Friend WithEvents btnVirgul As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents BtnEksi As Button
+    Friend WithEvents btnEsittir As Button
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btnBackSpace As Button
+    Friend WithEvents btnNegatif As Button
+
+End Class
---
+++ HesapMakinesi/HesapMakinesi/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class frmHesapMakinesi
+
+    Dim secim As Integer = 0
+    Dim sayi1, sayi2, sonuc As Double
+
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = ""
+        End If
+
+        Dim tiklananButon = CType(sender, Button)
+        txtDisplay.Text = txtDisplay.Text & tiklananButon.Text
+
+    End Sub
+
+
+    Private Sub btnBackSpace_Click(sender As Object, e As EventArgs) Handles btnBackSpace.Click
+        txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1)
+        If txtDisplay.Text = "" Then
+            txtDisplay.Text = "0"
+        End If
+    End Sub
+
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+
+        txtDisplay.Text = "0"
+
+    End Sub
+
+    Private Sub btnArti_Click(sender As Object, e As EventArgs) Handles btnArti.Click
+        sayi1 = Double.Parse(txtDisplay.Text)
+        secim = 1
+        txtDisplay.Text = "0"
+
+
+    End Sub
+
+    Private Sub BtnEksi_Click(sender As Object, e As EventArgs) Handles BtnEksi.Click
+        sayi1 = Double.Parse(txtDisplay.Text)
+        secim = 2
+        txtDisplay.Text = "0"
+
+
+    End Sub
+
+    Private Sub btnCarpi_Click(sender As Object, e As EventArgs) Handles btnCarpi.Click
+        sayi1 = Double.Parse(txtDisplay.Text)
+        secim = 3
+        txtDisplay.Text = "0"
+
+    End Sub
+
+    Private Sub btnBolu_Click(sender As Object, e As EventArgs) Handles btnBolu.Click
+        sayi1 = Double.Parse(txtDisplay.Text)
+        secim = 4
+        txtDisplay.Text = "0"
+
+    End Sub
+
+    Private Sub btnEsittir_Click(sender As Object, e As EventArgs) Handles btnEsittir.Click
+        sayi2 = Double.Parse(txtDisplay.Text)
+        If secim = 1 Then
+            sonuc = sayi1 + sayi2
+        End If
+        txtDisplay.Text = sonuc.ToString()
+
+        If secim = 2 Then
+            sonuc = sayi1 - sayi2
+        End If
+        txtDisplay.Text = sonuc.ToString()
+
+        If secim = 3 Then
+            sonuc = sayi1 * sayi2
+        End If
+        txtDisplay.Text = sonuc.ToString()
+
+        If secim = 4 Then
+            sonuc = sayi1 / sayi2
+        End If
+        txtDisplay.Text = sonuc.ToString()
+
+    End Sub
+
+    Private Sub btnVirgul_Click(sender As Object, e As EventArgs) Handles btnVirgul.Click
+        If txtDisplay.Text.IndexOf(",") < 1 Then
+            txtDisplay.Text += ","
+        End If
+
+    End Sub
+
+    Private Sub btnNegatif_Click(sender As Object, e As EventArgs) Handles btnNegatif.Click
+        If txtDisplay.Text <> "0" Then
+            If txtDisplay.Text.StartsWith("-") Then
+                txtDisplay.Text = txtDisplay.Text.Replace("-", "")
+            ElseIf txtDisplay.Text.StartsWith("") Then
+                txtDisplay.Text = "-" & txtDisplay.Text
+            End If
+        End If
+
+    End Sub
+
+End Class
